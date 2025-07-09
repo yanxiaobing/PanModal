2091911fc1bdee457800a586b78bd58bce533a07
xbingo <dove025@qq.com>
2025-07-09 20:44:37 +0800

    feat: fix ios 19

--- PanModal-Xbingo.podspec
+++ PanModal-Xbingo.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PanModal-Xbingo'
-  s.version          = '10.0.3'
+  s.version          = '10.0.4'
   s.summary          = 'Update PanModalPresentationController to support contentInsert'
 
 # This description is used to generate tags and improve search results.
@@ -22,7 +22,7 @@ Pod::Spec.new do |s|
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = {  "XBingo" => "dove025@qq.com"  }
   s.source           = { :git => 'https://github.com/yanxiaobing/PanModal.git', :tag => "#{s.version}" }
-  s.ios.deployment_target = '11.0'
+  s.ios.deployment_target = '14.0'
   s.swift_version = '5.0'
   s.source_files = 'PanModal/**/*.{swift,h,m}'
 end
--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -858,24 +858,22 @@ private extension PanModalPresentationController {
      */
     func addRoundedCorners(to view: UIView) {
         let radius = presentable?.cornerRadius ?? 0
-        let path = UIBezierPath(roundedRect: view.bounds,
-                                byRoundingCorners: [.topLeft, .topRight],
-                                cornerRadii: CGSize(width: radius, height: radius))
-
-        // Draw around the drag indicator view, if displayed
-        if presentable?.showDragIndicator == true {
-            let indicatorLeftEdgeXPos = view.bounds.width/2.0 - Constants.dragIndicatorSize.width/2.0
-            drawAroundDragIndicator(currentPath: path, indicatorLeftEdgeXPos: indicatorLeftEdgeXPos)
-        }
-
-        // Set path as a mask to display optional drag indicator view & rounded corners
-        let mask = CAShapeLayer()
-        mask.path = path.cgPath
-        view.layer.mask = mask
-
-        // Improve performance by rasterizing the layer
+        view.layer.cornerRadius = radius
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        view.layer.masksToBounds = true
         view.layer.shouldRasterize = true
         view.layer.rasterizationScale = UIScreen.main.scale
+        
+        if #available(iOS 19.0, *) {
+            let subviews = view.subviews
+            view.subviews.forEach { $0.removeFromSuperview() }
+            subviews.forEach { view.addSubview($0) }
+            
+            DispatchQueue.main.async {
+                view.layer.cornerRadius = radius
+                view.layer.masksToBounds = true
+            }
+        }
     }
 
     /**
